57ed39927c891e294d909ed4d43587bc2cc136a7
Sam <samweldon9393@gmail.com>
2025-02-12 23:33:34 -0500

    break address collumn into address and address notes:

--- data.py
+++ data.py
@@ -1,5 +1,32 @@
 import numpy as np
 import pandas as pd
 
+def split_address_notes(string):
+    strings = string.split('[')
+    if len(strings) == 1:
+        return [string, None]
+    if string[-1] == ']':
+        addr = strings[0][:-1]
+        note = strings[1][:-1]
+    else:
+        split = strings[1].split(']')
+        note = split[0]
+        addr = strings[0] + split[1]
+    return addr, note 
+
+def split_addr_col(col):
+    new_col = []
+    for string in col:
+        new_col.append(split_address_notes(string))
+    return new_col
+
+
 df = pd.read_csv('./providers.csv')
-print(df.describe())
+#print(df['address'])
+new_col = split_addr_col(df['address'])
+new_df = pd.DataFrame(new_col)
+
+df['address'] = new_df[0]
+df['address note'] = new_df[1]
+
+print(df.head())
