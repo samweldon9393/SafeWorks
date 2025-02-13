6056cbeaf5d68647a1c6fc921847b71b1e0cd7f9
Sam <samweldon9393@gmail.com>
2025-02-12 23:01:35 -0500

    loading csv into pandas all good

--- data.py
+++ data.py
@@ -1,7 +1,5 @@
 import numpy as np
-import pands as pd
+import pandas as pd
 
-#df = pd.read_csv('providers.csv')
-arr = np.array([1, 2, 3])
-print(arr)
-#print(df.head())
+df = pd.read_csv('./providers.csv')
+print(df.describe())
