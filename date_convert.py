5393e7964aa0ec93ff29b567e61c964a61d79dfe
Sam <samweldon9393@gmail.com>
2025-02-13 22:24:51 -0500

    start writing functin to split hours strings

--- date_convert.py
+++ date_convert.py
@@ -1,2 +1,18 @@
+import numpy as np
 import pandas as pd 
+import re
+
+def split_days(string):
+    split = re.split(r'[;,\s]', string)
+    for i, s in enumerate(split):
+        if s == '':
+            continue
+        else:
+            print(s)
+#df = pd.read_csv('./providers_2.csv')
+
+#hours = df['hours']
+
+test = "Mon, Wed, Fri 1 pm 4 pm; Tue 9 am 4 pm; Thu 1 pm 6 pm"
+print(split_days(test))
 
