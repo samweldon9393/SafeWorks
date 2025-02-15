6a9fdd68eb548f7ae4c11b5ea0bedd79fdff84be
Sam <samweldon9393@gmail.com>
2025-02-14 21:14:35 -0500

    dates converted into dictionary, then to json, saved to sqlite

--- date_convert.py
+++ date_convert.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd 
 import re
-
+import sqlite3
+import json
 
 days = {
         "Mon" : "Monday",
@@ -26,10 +27,20 @@ hours_template = {
         }
 
 def split_days(string):
+
+
     hours = hours_template.copy()
+
+    if "Please" in string:
+        for day in hours:
+            hours[day] = "Please call"
+        return hours
+
     string = string.replace(":", "")
     split = re.split(r'[;,\s]+', string)
     i = 0
+    
+
     while i < len(split):
         queue = []
         times = []
@@ -65,17 +76,36 @@ def split_days(string):
 
     return hours
 
+def convert_col(col):
+    new_col = []
+    for string in col:
+        new_col.append(split_days(string))
+    return new_col
 
 
-#df = pd.read_csv('./providers_2.csv')
+
+
+df = pd.read_csv('./providers_4.csv')
 
 #hours = df['hours']
+#df['hours'] = convert_col(df['hours'])
+df['hours'] = df['hours'].apply(json.dumps)
 
+conn = sqlite3.connect('providers.db')
+
+df.to_sql("providers", conn, if_exists="replace", index=False)
+conn.close()
+
+'''
+df.to_csv('providers_4.csv')
 test = "Mon, Wed, Fri 1 pm 4 pm; Tue 9 am 4 pm; Thu 1 pm 6 pm"
 test2 = "Mon, Tue, Thu 9 am 1 pm; Fri 1 pm 7 pm"
 test3 = "Wed 10 am 3 pm"
 test4 = "Mon through Fri 10:30 am 5:30 pm"
+test5 = "Thu Please Call"
 print(split_days(test))
 print(split_days(test2))
 print(split_days(test3))
 print(split_days(test4))
+print(split_days(test5))
+'''
